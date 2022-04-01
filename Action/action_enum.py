5455f9e97a848d2754c671e3611fc396e291c056
rakeshMolakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-03-31 22:29:03 -0400

    Add files via upload

---
+++ Action/action_enum.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class Actions(Enum):
+    """
+    Actions enum
+    """
+    
+    # walk = 0
+    # sleep = 1
+    # sit = 2
+    # falldown = 3
+    # ignore = 4
+
+    eat = 0
+    falldown = 1
+    standd = 2
+    suicide = 3
+    stand = 4
+    
